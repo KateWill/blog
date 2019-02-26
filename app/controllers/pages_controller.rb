7c205a573b49a60eee76df60b508b0f9743ec52d
Katelyn Williamson <katelynrwilliamson@gmail.com>
2019-02-25 20:58:19 -0700

    end of practice

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,4 +15,42 @@ class PagesController < ApplicationController
     # create a new instance
     @page = Page.new #in memory
   end
+
+  def create
+    @page = Page.new(pages_params)
+    if @page.save
+      redirect_to pages_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    #renders the form only
+    @page = Page.find(params[:id])
+  end
+
+  def update
+    @page = Page.find (params[:id])
+    if @page.update(pages_params)
+      redirect_to pages_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Page.find(params[:id]).destroy
+    redirect_to pages_path
+  end
+
+
+  private
+    def pages_params
+      params.require(:page).permit(:title, :author, :body)
+    end
+    # {page: {title: 'foo', author: 'me', body: 'this'}}
+    # {title: 'foo', author: 'me', body: 'this'}
+    # Page.new({title: 'foo', author: 'me', body: 'this'})
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  # get 'static_pages/home'
+  # get 'static_pages/about'
+  root 'static_pages#home'
+
+  get '/about', to: 'static_pages#about'
   # get 'pages/index'
   # get 'pages/show'
   # get 'pages/new'
   resources :pages
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
